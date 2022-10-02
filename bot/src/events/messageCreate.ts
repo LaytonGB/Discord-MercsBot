7adbfbfbaa943b2df372021d70fd10dc7e30f011
Layton Burchell <laytonb@hotmail.co.uk>
2022-10-02 15:39:50 +0100

    🚧 PROGRESS: set up bot message

--- bot/src/events/messageCreate.ts
+++ bot/src/events/messageCreate.ts
@@ -1,4 +1,12 @@
-import { Client, Message } from "discord.js";
+import { Message } from "discord.js";
+
+const paramsToUrl = (params: { [key: string]: string }): string => {
+  const s: string[] = [];
+  for (const key in params) {
+    s.push(`${key}=${params[key]}`);
+  }
+  return s.join("&");
+};
 
 module.exports = {
   name: "messageCreate",
@@ -6,9 +14,12 @@ module.exports = {
     if (rawMessage.author.bot) return;
 
     const msg = await rawMessage.fetch(true);
-    const match = msg.content.match(/<t:\d{10}:\w>/);
+    const match = msg.content.match(/(?<=session.*)<t:\d{10}:\w>/i);
     if (match?.length === 1) {
-      msg.reply(match[0]);
+      const embed = match[0];
+      const timestamp = embed.split(":")[1];
+      msg.reply(`**Calendar Links for Session ${embed}**\n` +
+        `https://laytongb.gitlab.io/discord-mercsbot?${paramsToUrl({ timestamp })}`);
     }
   },
 };
